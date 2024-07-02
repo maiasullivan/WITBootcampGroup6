037e8b9de73d72371481db5c5c559d4a95f875ee
maiasullivan <s3942603@student.rmit.edu.au>
2024-07-02 14:32:31 +1000

    pages as files

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import React from "react";
+import './index.css'
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import SignUp from './components/Sign-Up/Signup.tsx'
+import Login from './components/Login/Login.tsx'
 import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
