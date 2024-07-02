9a51a78e040cc1890426a6a27e31df7e919a0169
maiasullivan <s3942603@student.rmit.edu.au>
2024-07-02 15:29:55 +1000

    Added dashboard as home page

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import './index.css'
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App.tsx";
+import Dashboard from "./components/Dashboard/Dashboard.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
@@ -10,7 +12,10 @@ Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-    
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
